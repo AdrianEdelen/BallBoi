47e6f6578d3ebef30672bd527d333d1cec60b8fe
Adrian Edelen <adrianedelen@gmail.com>
2023-03-25 16:21:59 -0400

    added a comment for testing purposes

--- BallBoi/Program.cs
+++ BallBoi/Program.cs
@@ -18,7 +18,7 @@ public class Program
     public async Task MainAsync()
     {
 
-
+        //Here is a comment I added for testing
 
         //Things the bot can do: Ideas
         //music playlist, remove the embed, after x time, rewrite it to a regular
